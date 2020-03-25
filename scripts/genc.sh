a29878c7105b8b4da13ab604b417024e86106bf1
evlon <liamgnc@gmail.com>
2020-03-25 12:49:12 +0800

    update ci

--- scripts/genc.sh
+++ scripts/genc.sh
@@ -1,7 +1,14 @@
-echo -n [ >.cache
+if [ ! -d cache ] ;then mkdir cache;fi
+git remote rm origin
+git remote add orgin "https://${username}:${password}@github.com/${username}/${repositoryName}.git"
+
+echo -n [ > cache/.cache
 for i in $(find ./ -name .url);  
-    do echo -n '{"f":"'$(echo $i | sed 's/^\.\///' | sed 's/\/\.url$//')'","info":'$(cat $i)'},' >> .cache ;
+    do echo -n '{"f":"'$(echo $i | sed 's/^\.\///' | sed 's/\/\.url$//')'","info":'$(cat $i)'},' >> cache/.cache ;
 done 
-cat .cache | sed 's/,$//' > .cache.json 
-echo -n ] >> .cache.json
-rm .cache
+cat cache/.cache | sed 's/,$//' > cache/.cache.json 
+echo -n ] >> cache/.cache.json
+rm cache/.cache
+
+git push github `git subtree split --prefix examples master`:gh-pages --force
+
