1756d91c9d5a0a624f1f77c491064a2a1aa6980a
evlon <liamgnc@gmail.com>
2020-03-25 23:00:58 +0800

    update ci

--- scripts/genc.sh
+++ scripts/genc.sh
@@ -1,6 +1,8 @@
-if [ ! -d cache ] ;then mkdir cache;fi
-git remote add rcache "https://${username}:${password}@github.com/${username}/${repositoryName}.git"
 
+git remote remove origin
+git remote add rcache "https://cdndb:${password}@github.com/cdndb/cto.git"
+git pull origin master
+if [ ! -d cache ] ;then mkdir cache;fi
 echo -n [ > cache/.cache
 for i in $(find ./ -name .url);  
     do echo -n '{"f":"'$(echo $i | sed 's/^\.\///' | sed 's/\/\.url$//')'","info":'$(cat $i)'},' >> cache/.cache ;
@@ -8,7 +10,7 @@ done
 cat cache/.cache | sed 's/,$//' > cache/.cache.json 
 echo -n ] >> cache/.cache.json
 rm cache/.cache
-git checkout cache
-git add cache/*
+git switch cache
+git add -A
 git commit -m '[skip travis] build cache'
-git push rcache cache -f
+git push origin cache
