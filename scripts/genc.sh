aa704039d46db95bd1ca0a7a62156eceb73f194c
evlon <liamgnc@gmail.com>
2020-03-26 09:41:59 +0800

    update ci

--- scripts/genc.sh
+++ scripts/genc.sh
@@ -1,17 +1,21 @@
 
 git remote remove origin
 git remote add origin "https://cdndb:${password}@github.com/cdndb/cto.git"
+git fetch origin cache
 git pull origin master
-git pull origin cache
+
 if [ ! -d cache ] ;then mkdir cache;fi
 echo -n [ > cache/.cache
 for i in $(find ./ -name .url);  
     do echo -n '{"f":"'$(echo $i | sed 's/^\.\///' | sed 's/\/\.url$//')'","info":'$(cat $i)'},' >> cache/.cache ;
 done 
-cat cache/.cache | sed 's/,$//' > cache/.cache.json 
-echo -n ] >> cache/.cache.json
+cat cache/.cache | sed 's/,$//' > cache/cache.json 
+echo -n ] >> cache/cache.json
 rm cache/.cache
+mv cache ../cache
+
+git switch cache -f
+cp -rf ../cache/* cache/ 
 git add -A
-git switch cache
 git commit -m '[skip travis] build cache'
 git push origin cache
